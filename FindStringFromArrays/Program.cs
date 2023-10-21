c0ef5db7e5d0518af841cf4fe63cac6985cf5a8a
Manali Renavikar <manalirenavikar@gmail.com>
2023-10-21 21:01:13 +0100

    Add project files.

---
+++ FindStringFromArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindStringFromArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+            string[] arr = new string[] { "cat", "dog", "monkey", "human", "elephant" };
+            for (int i = 0; i <= str.Length; i++)
+            {
+                if (str==arr[i])
+                {
+                    Console.WriteLine("String found" + arr[i]);
+                }
+                else 
+                { Console.WriteLine("String not in list"); 
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
